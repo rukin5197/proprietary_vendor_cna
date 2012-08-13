fbc852db4c5212affc30e9369563c51f5bda9deb
Codenamedroid <bemegadroid@gmail.com>
2012-08-13 17:00:19 -0500

    overlaid the wrong folder -_-

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
-PRODUCT_PACKAGE_OVERLAYS += vendor/cna/overlay/tablet
-
 # Inherit common stuff
 $(call inherit-product, vendor/cna/config/common.mk)
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/cna/overlay/tablet
 
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)

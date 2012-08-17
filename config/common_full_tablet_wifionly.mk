3a819d6607657c98d94b926c551532d0a402e043
Codenamedroid <bemegadroid@gmail.com>
2012-08-17 14:58:36 -0500

    pull in bluetooth configs per device

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -3,5 +3,9 @@ $(call inherit-product, vendor/cna/config/common.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/cna/overlay/tablet
 
+# BT config
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
+
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
